ba6a434f21c38404240d342c8aa2606b722d4181
Nimely111 <samuelehnimely@gmail.com>
2025-01-11 23:00:26 +0000

    re organize the user router as camelCase and the userRouter to the main application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from app.api.routers import router as UserRouter
+from app.api.routers import router as userRouter
 
 app = FastAPI(title="Try and See Financial Management system Backend", docs_url="/docs")
+
+app.include_router(userRouter)
 
 @app.get("/")
 async def read_root():
