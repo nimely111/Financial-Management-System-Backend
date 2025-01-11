bfb17ca77f38d4051d770654f6e79de489886c86
Nimely111 <samuelehnimely@gmail.com>
2025-01-11 23:07:50 +0000

    imported the base and engine to be use on the main app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.api.routers import router as userRouter
+from app.db.db_setup import Base, engine
+
 
 app = FastAPI(title="Try and See Financial Management system Backend", docs_url="/docs")
 
+Base.metadata.create_all(bind = engine)
 app.include_router(userRouter)
 
 @app.get("/")
