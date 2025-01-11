0439c94d923405fc1e28fb8d25aded65454c0f75
Nimely111 <samuelehnimely@gmail.com>
2025-01-11 22:36:44 +0000

    removed unnecessary importation and import the user router into the main application

--- app/main.py
+++ app/main.py
@@ -1,19 +1,5 @@
 from fastapi import FastAPI
-from db.db_setup import get_db
-from models.models import User, Transaction
-from schemas.users import (
-    UserBase,
-    UserCreate,
-    UserResponse,
-    UserUpdate
-)
-
-from schemas.transactions import (
-    TransactionBase,
-    TransactionCreate,
-    TransactionResponse,
-    TransactionUpdate
-    )
+from app.api.routers import router as UserRouter
 
 app = FastAPI(title="Try and See Financial Management system Backend", docs_url="/docs")
 
