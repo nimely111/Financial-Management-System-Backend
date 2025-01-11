4ec0d0513da5a71771f6ecbc1cfe4f4f3f998ab7
Nimely111 <samuelehnimely@gmail.com>
2025-01-11 14:57:14 +0000

    import models and schemas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from db.db_setup import get_db
-
+from models.models import User, Transaction
+from schemas.users import (
+    UserBase,
+    UserCreate,
+    UserResponse,
+    UserUpdate
+)
 app = FastAPI(title="Try and See Financial Management system Backend", docs_url="/docs")
 
 @app.get("/")
