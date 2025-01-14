354ed8e3477fb304b6276a57de69f12c8cd1210f
Nimely111 <samuelehnimely@gmail.com>
2025-01-14 15:53:02 +0000

    change deprecated python dict method to model_dump() method

--- app/crud/users.py
+++ app/crud/users.py
@@ -10,7 +10,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 10):
     return db.query(User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: UserCreate):
-    db_user = User(**user.dict())
+    db_user = User(**user.model_dump())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
