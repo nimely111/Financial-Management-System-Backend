cfae5c9a48c4a5a8dc8f8addab0c3c7e073dd8f4
Nimely111 <samuelehnimely@gmail.com>
2025-01-14 17:01:42 +0000

    initialize API router with necessary info for api docs

--- app/api/routers/transactions.py
+++ app/api/routers/transactions.py
@@ -3,3 +3,8 @@ from sqlalchemy.orm import Session
 from db.db_setup import get_db
 from crud.transactions import get_transactions, get_transaction, create_transaction, update_transaction, delete_transaction
 from schemas.transactions import TransactionCreate, TransactionUpdate, Transaction
+
+router = APIRouter(
+    prefix="/transactions",
+    tags=["transactions"]
+)
