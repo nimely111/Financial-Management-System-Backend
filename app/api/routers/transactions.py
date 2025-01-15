f0dbd8f3bebaddea2edfdfe82206a96ccbf8d70e
Nimely111 <samuelehnimely@gmail.com>
2025-01-15 05:10:55 +0000

    update Transactions to TransactionResponse in the transaction route for better response of transaction data

--- app/api/routers/transactions.py
+++ app/api/routers/transactions.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db.db_setup import get_db
 from crud.transactions import get_transactions, get_transaction, create_transaction, update_transaction, delete_transaction
-from schemas.transactions import TransactionCreate, TransactionUpdate, Transaction
+from schemas.transactions import TransactionCreate, TransactionUpdate, TransactionResponse
 
 router = APIRouter(
     prefix="/transactions",
