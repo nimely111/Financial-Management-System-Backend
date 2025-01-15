892773ce28e96643f8878463e8647308a1c5b1fa
Nimely111 <samuelehnimely@gmail.com>
2025-01-15 09:16:57 +0000

    import the trasactions router into the main application

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from api.routers.users import router as userRouter
+from api.routers.transactions import router as transactionRouter
 from db.db_setup import Base, engine
 
 app = FastAPI(title="Try and See Financial Management system Backend", docs_url="/docs")
 
 Base.metadata.create_all(bind = engine)
 app.include_router(userRouter)
+app.include_router(transactionRouter)
 
 @app.get("/")
 async def read_rootsss():
