d5a3de3f88b62a9e4411fd6fcd92507c0c0307a9
Nimely111 <samuelehnimely@gmail.com>
2025-01-17 20:23:51 +0000

    setup CORS middleware credentials to only allow frontend applications that are permitted to consume the backend logic possible

--- app/main.py
+++ app/main.py
@@ -17,6 +17,13 @@ app.include_router(transactionRouter)
 async def read_root():
     return {"status": "200"}
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
