dbb335ab4e8d3a43863f4c0cdfb360bd6a7b4061
Nimely111 <samuelehnimely@gmail.com>
2025-01-24 08:19:09 +0000

    add comment for route simplicity

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -9,11 +9,12 @@ router = APIRouter(
     tags=["Users"]
 )
 
-
+# fetch all users
 @router.get("/", response_model=list[UserResponse])
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return get_users(db, skip=skip, limit=limit)
 
+# fetch a specific user by id
 @router.get("/{user_id}", response_model=UserResponse)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     user = get_user(db, user_id)
