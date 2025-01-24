af3a8623aaee1dfdb8a9ff20df42a785ebd09840
Nimely111 <samuelehnimely@gmail.com>
2025-01-24 08:16:52 +0000

    join the transactions table to the user table to synchronize them for our  query

--- app/crud/users.py
+++ app/crud/users.py
@@ -7,7 +7,7 @@ def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(User).offset(skip).limit(limit).all()
+    return db.query(User).options(joinedload(User.transactions)).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: UserCreate):
     db_user = User(**user.model_dump())
