f577f0f03d1881ed11c34e13d3a9a3f59c48b21c
Nimely111 <samuelehnimely@gmail.com>
2025-01-24 09:33:46 +0000

    update the main.py file to include the app variable for the python run function

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app", 
+        app = "app.main:app", 
         host="0.0.0.0", 
         port=8000, 
         reload=True
